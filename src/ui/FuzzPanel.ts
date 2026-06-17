255a444e5aee1a7e3d34232a91bf1379d057dc03
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-06-17 14:42:26 -0400

    chore: update ideas panel text

--- src/ui/FuzzPanel.ts
+++ src/ui/FuzzPanel.ts
@@ -2476,7 +2476,7 @@ ${src}`;
           icon: "codicon-lightbulb",
           payload: "ideasGrid",
           description:
-            "Some of the ideas below, which are based on the last test run, might help you improve the test suite.",
+            "Some of the ideas below, which are based on the last test run, might help you improve the test suite. The ideas are based on the last test run and might not reflect changes you made subsequently.",
         });
 
         // If the prior tab no longer exists in the display set, don't use it
